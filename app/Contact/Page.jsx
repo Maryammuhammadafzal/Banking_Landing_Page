c7f990b5cb0c9fad62cb128542a5cd08d1cf1814
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-17 17:10:40 +0500

    creating contact section responsive

--- app/Contact/Page.jsx
+++ app/Contact/Page.jsx
@@ -6,10 +6,6 @@ import Image from "next/image";
 const ContactPage = () => {
   return (
     <div className="w-full lg:min-h-screen font-sans relative overflow-hidden h-auto flex justify-center items-center">
-      <div
-        className="bg-center absolute top-0 -z-10 left-0 opacity-45 bg-cover w-full h-full"
-        style={{ backgroundImage: `url(${HeroBg.src})` }}
-      ></div>
       <div className=" w-full h-full flex justify-center items-center">
         <div className="lg:w-[90%] w-full px-3 h-full items-center justify-start flex flex-col gap-4">
             <div className="flex md:flex-row flex-col w-full h-full items-center ">
@@ -32,15 +28,15 @@ const ContactPage = () => {
                 </p>
               </div>
 
-              <div className="md:w-[40%] w-full h-auto relative justify-end flex">
+              <div className="md:w-[45%] w-full h-auto relative justify-end flex">
                 <Image
                   src={ContactImage}
                   alt="hero-image"
-                  width={450}
-                  height={480}
-                  className="translate-y-9  transition-all ease-linear w-[450px]"
+                  width={690}
+                  height={529}
+                  className="z-20 scale-x-[-1] w-[1000px]"
                 />
-                <div className="xl:min-w-[370px] md:w-[320px] md:h-[320px] xl:h-[370px] rounded-full bg-[#C7D216] -right-30 -z-10 absolute -bottom-20"></div>
+                <div className="xl:min-w-[370px] md:w-[320px] md:h-[320px] xl:h-[370px] rounded-full bg-[#C7D216] -right-30  absolute -bottom-20"></div>
               </div>
             </div>
         </div>
