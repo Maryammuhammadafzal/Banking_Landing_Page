a059cd629f4a46cca029ced9381805c8143ad428
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-17 14:52:34 +0500

    creating hero section responsive

--- app/Hero/Page.jsx
+++ app/Hero/Page.jsx
@@ -15,8 +15,8 @@ const HeroPage = () => {
         <div className="lg:w-[90%] w-full px-3 h-full items-center justify-start flex flex-col gap-4">
           <Header />
           <div className="flex md:flex-row flex-col w-full h-full items-center ">
-            <div className="md:w-[60%] w-full 2xl:pr-12  h-auto flex flex-col gap-3">
-              <h1 className="font-extrabold max-md:max-w-lg max-w-2xl xl:text-6xl md:text-5xl sm:text-4xl text-3xl 2xl:text-[64px] 2xl:leading-24 lg:leading-18 sm:leading-14 leading-10  uppercase">
+            <div className="md:w-[60%] w-full  h-auto flex flex-col gap-3">
+              <h1 className="font-extrabold xl:text-6xl md:text-5xl max-w-3xl pr-10 sm:text-5xl xs:text-4xl text-3xl 2xl:text-[64px] 2xl:leading-24 lg:leading-18 sm:leading-14 leading-10  uppercase">
                 Discover the Future of Banking with FutureBank
               </h1>
               <Image
@@ -24,7 +24,7 @@ const HeroPage = () => {
                 alt="border"
                 width={400}
                 height={70}
-                className=""
+                className="w-[50%]"
               />
             </div>
             <div className="md:w-[40%] w-full h-auto relative justify-end flex">
@@ -33,7 +33,7 @@ const HeroPage = () => {
                 alt="hero-image"
                 width={450}
                 height={480}
-                className="translate-y-9 transition-all ease-linear "
+                className="translate-y-9 transition-all ease-linear w-[450px]"
               />
               <div className="xl:min-w-[370px] md:w-[320px] md:h-[320px] xl:h-[370px] rounded-full bg-[#C7D216] -right-30 -z-10 absolute -bottom-20"></div>
             </div>
