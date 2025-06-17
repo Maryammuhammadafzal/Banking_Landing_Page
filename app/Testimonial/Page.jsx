13f3842261b1abff1e66ef61b5a80968d41386be
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-17 16:17:35 +0500

    creating testimonial section responsive

--- app/Testimonial/Page.jsx
+++ app/Testimonial/Page.jsx
@@ -19,18 +19,9 @@ const TestimonialPage = () => {
         </div>
         <p className="text-black text-lg">At  FutureBank Online, we’re committed to helping you achieve your financial goals. From saving for a dream vacation to managing your investments, we’re here to support you every step of the way.</p>
       </div>
-      <div className="partner-card bg-[#E6E6E6] w-[40%] h-auto pl-14 pr-6 rounded-tl-4xl rounded-bl-4xl flex flex-col justify-center gap-3 py-8">
+      <div className="customer-card bg-[#E6E6E6] w-[40%] h-auto pl-6 pr-6 rounded-tl-4xl rounded-bl-4xl flex flex-col justify-center gap-3 py-8">
         <div className=" w-full  h-auto flex flex-col gap-3">
-          <h1 className="font-extrabold xl:text-6xl md:text-5xl max-w-3xl lg:pr-14 sm:text-5xl xs:text-4xl text-3xl 2xl:text-[64px] 2xl:leading-22 lg:leading-18 sm:leading-14 leading-10  uppercase">
-            Your Financial Partner
-          </h1>
-          <Image
-            src={HeadingBorder2}
-            alt="border"
-            width={400}
-            height={70}
-            className="w-[50%]"
-          />
+          <Image/>  <h2 className="font-bold text-2xl">Customer Support</h2>
         </div>
         <p className="text-black text-lg">At  FutureBank Online, we’re committed to helping you achieve your financial goals. From saving for a dream vacation to managing your investments, we’re here to support you every step of the way.</p>
       </div>
