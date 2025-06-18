ea1789afa5c2970f72be9302f9cb019a9dfb6f86
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-18 11:06:20 +0500

    responsive

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <div className="w-[90%] flex flex-col gap-5 items-center">
         <div className="flex flex-col items-center gap-4 py-10 w-full h-auto ">
           <div className="w-full h-auto flex flex-col gap-3 ">
-            <h1 className="font-extrabold text-neutral-100 text-center w-full xl:text-6xl md:text-5xl max-w-3xl sm:text-5xl xs:text-4xl text-3xl 2xl:text-[170px] 2xl:leading-40 lg:leading-18 sm:leading-14 leading-10  uppercase">
+            <h1 className="font-extrabold text-neutral-100 text-center w-full xl:text-6xl md:text-5xl max-w-3xl sm:text-4xl xs:text-4xl text-4xl 2xl:text-[170px] 2xl:leading-40 lg:leading-18 sm:leading-14 leading-10  uppercase">
               FutureBank
             </h1>
             <Image
@@ -22,8 +22,8 @@ const Footer = () => {
             />
           </div>
           <div className=" w-full h-auto py-3 flex  flex-col justify-center items-center gap-6">
-            <h2 className="text-4xl font-bold uppercase">FOllow Us On</h2>
-            <div className="flex justify-center text-[#C7D216] text-5xl gap-6 items-center">
+            <h2 className="md:text-4xl sm:text-3xl text-2xl font-bold uppercase">FOllow Us On</h2>
+            <div className="flex justify-center text-[#C7D216] md:text-5xl sm:text-4xl text-3xl gap-6 items-center">
               <MdOutlineFacebook />
               <FaXTwitter />
               <FaLinkedin />
@@ -32,9 +32,9 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="copyright flex justify-between sm:flex-row md:flex-col items-center w-full py-2">
-          <p className="md:text-sm text-md">
-            © 2021 all copyright reserved. <a href="">Rootlet Solution</a>.
+        <div className="copyright flex justify-between md:flex-row gap-3 flex-col items-center w-full py-2">
+          <p className="md:text-sm text-xs">
+            © 2021 all copyright reserved. <a href="" className="underline">Rootlet Solution</a>.
           </p>
           <ul className="flex gap-3 md:text-lg text-sm">
             {" "}
