0eed5a02eb8f74e372340e5381f2cc5e4ccd53a2
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-18 10:23:05 +0500

    completed

--- app/Contact/Page.jsx
+++ app/Contact/Page.jsx
@@ -7,9 +7,9 @@ const ContactPage = () => {
   return (
     <div className="w-full lg:min-h-screen font-sans relative overflow-hidden h-auto flex justify-center items-center">
       <div className=" w-full h-full flex justify-center items-center">
-        <div className="lg:w-[90%] w-full px-3 h-full items-center justify-start flex flex-col gap-4">
+        <div className="w-full px-3 h-full items-center justify-start flex flex-col gap-4">
             <div className="flex md:flex-row flex-col w-full h-full items-center ">
-              <div className="md:w-[60%] w-full  h-auto flex flex-col gap-3">
+              <div className="md:w-[60%] lg:pl-20 w-full  h-auto flex flex-col gap-3">
                 <h1 className="font-extrabold xl:text-6xl md:text-5xl max-w-3xl pr-10 sm:text-5xl xs:text-4xl text-3xl 2xl:text-[64px] 2xl:leading-24 lg:leading-18 sm:leading-14 leading-10  uppercase">
                   Join the FutureBank Family Today!
                 </h1>
@@ -28,15 +28,15 @@ const ContactPage = () => {
                 </p>
               </div>
 
-              <div className="md:w-[45%] w-full h-auto relative justify-end flex">
+              <div className="md:w-[45%] w-full h-auto relative justify-end items-end flex">
                 <Image
                   src={ContactImage}
                   alt="hero-image"
-                  width={690}
+                  width={750}
                   height={529}
-                  className="z-20 scale-x-[-1] w-[1000px]"
+                  className="z-20 w-[760px]"
                 />
-                <div className="xl:min-w-[370px] md:w-[320px] md:h-[320px] xl:h-[370px] rounded-full bg-[#C7D216] -right-30  absolute -bottom-20"></div>
+                <div className="xl:min-w-[370px] md:w-[320px] md:h-[320px] xl:h-[370px] rounded-full bg-[#C7D216] -right-10  absolute bottom-10"></div>
               </div>
             </div>
         </div>
--- app/Testimonial/Page.jsx
+++ app/Testimonial/Page.jsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 const TestimonialPage = () => {
   return (
-    <div className="w-full h-auto py-20 flex items-center gap-5">
-      <div className="partner-card bg-[#C7D216] w-[65%] h-auto pl-14 pr-6 rounded-tr-4xl rounded-br-4xl flex flex-col justify-center gap-3 py-8">
+    <div className="w-full h-auto py-20 flex md:flex-row flex-col items-center gap-2 lg:gap-5">
+      <div className="partner-card bg-[#C7D216] w-[95%] md:w-[65%] min-h-[400px] max-sm:px-3 pl-14 pr-6 rounded-tr-4xl rounded-br-4xl flex flex-col justify-center gap-3 py-8">
         <div className=" w-full  h-auto flex flex-col gap-3">
           <h1 className="font-extrabold xl:text-6xl md:text-5xl max-w-3xl lg:pr-14 sm:text-5xl xs:text-4xl text-3xl 2xl:text-[64px] 2xl:leading-22 lg:leading-18 sm:leading-14 leading-10  uppercase">
             Your Financial Partner
@@ -19,15 +19,15 @@ const TestimonialPage = () => {
             className="w-[50%]"
           />
         </div>
-        <p className="text-black text-lg">At  FutureBank Online, we’re committed to helping you achieve your financial goals. From saving for a dream vacation to managing your investments, we’re here to support you every step of the way.</p>
+        <p className="text-black text-base sm:text-lg">At  FutureBank Online, we’re committed to helping you achieve your financial goals. From saving for a dream vacation to managing your investments, we’re here to support you every step of the way.</p>
       </div>
-      <div className="customer-card bg-[#E6E6E6] w-[35%] h-auto pl-8 pr-8 rounded-tl-4xl rounded-bl-4xl flex flex-col justify-center gap-7 py-8">
+      <div className="customer-card bg-[#E6E6E6] w-[95%] md:w-[35%] min-h-[400px] max-sm:px-3 max-md:pl-14 max-md:pr-6 lg:pl-8 lg:pr-8 px-3 rounded-tl-4xl rounded-bl-4xl flex flex-col justify-center gap-7 py-8">
         <div className=" w-full  h-auto items-center flex gap-3">
           <Image src={CustomerSupportIcon} width={70} height={70} alt="icon"
           />  <h2 className="font-extrabold text-3xl uppercase">Customer Support</h2>
         </div>
-        <p className="text-black text-lg">We’re here for you 24/7. Whether you have a question about a transaction or need assistance with our platform, our dedicated customer service team is just a call or click away.</p>
-      <div className="py-3"><Button className='bg-[#C7D216]'>Contact Now</Button></div>
+        <p className="text-black text-base lg:text-lg">We’re here for you 24/7. Whether you have a question about a transaction or need assistance with our platform, our dedicated customer service team is just a call or click away.</p>
+      <div className=""><Button className='bg-[#C7D216]'>Contact Now</Button></div>
       </div>
     </div>
   );
